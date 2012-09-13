ea3653ea2aeb4aab01aefe22ef49d9d6e3de58fb
liuna <liuna.mailbox@gmail.com>
2012-09-13 17:26:18 +0800

    add siber2

--- components/view.php
+++ components/view.php
@@ -10,8 +10,8 @@
  <!--   	<a href="javascript:parentAccordion.pr(1)">Exand All</a> | <a href="javascript:parentAccordion.pr(-1)">Collapse All</a>
 -->
 	<?php include_once(dirname(__FILE__)."/../includes/sidebar.php")?>
-
     </div>
+    
     <div id="content">
 	<div class="border"></div>
 	<div id="content2">
@@ -30,4 +30,15 @@
 	<p>野生大麻，特別是尋常大麻（Cannabis sativa L.var.indica Lamarck）在全球廣泛存在，生長地區包括吉爾吉斯坦、阿富汗，中國部分地區、匈牙利、波蘭、保加利亞、印度和尼泊爾。另有「印度大麻」（Cannabis indica）主要生長在阿富汗、美屬維爾京群島和摩洛哥。各地的野生印度大麻基因成分不同。例如在溫暖地帶，野生印度大麻能長到六米高，但較為寒冷的地方有時候只有30厘米高。幾乎每朵苞片花都結有種子。野生尋常大麻花苞細長分散，類似聖誕樹形狀。野生印度大麻則多密集枝條，花苞粗壯，結子較少。[1] 在很多地方，野生大麻因為政府清除和城市化，數量受到威脅。</p>
 	</div>
     </div>
-  </div>
+    
+    <div id="sidebar2">
+  	<div class="border"></div>
+    	<div id="side2Content">
+        	<h2 style="font-size:20px; font-weight:bold; text-align:center;">相关知识介绍</h2></br>
+			<p>大麻（學名：Cannabis sativa），又稱尋常大麻，又名線麻、白麻，中國古稱漢麻、火麻、枲、苴，大麻科大麻属一年生草本植物。生長地區包括在亞洲的吉爾吉斯坦、阿富汗，中國部分地區、印度和尼泊爾，以及歐洲的匈牙利、波蘭、保加利亞等地。
+			</p><br/>
+			<p>人類長久以來，種植這種草本植物，作為工業纖維，植物油脂、宗教用途以及藥用之上。它的種子，被稱為火麻仁或大麻仁，中醫使用它來治療便秘、腹瀉，可以被用來作成大麻籽油（Hemp oil）
+            </p></br>
+        </div>
+  	</div>
+</div>
